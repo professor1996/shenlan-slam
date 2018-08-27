3663f366df2493dbde8308c7c93426e27ef4d2fd
pengrui <983091438@qq.com>
2018-08-27 16:47:08 +0800

    1

---
+++ 第1讲/3.cmake练习/CMake_Practice/build/CMakeFiles/helloSLAM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloSLAM.dir/src/hello.cpp.o"
+  "libhelloSLAM.pdb"
+  "libhelloSLAM.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloSLAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 第1讲/3.cmake练习/CMake_Practice/build/CMakeFiles/hiSLAM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hiSLAM.dir/src/useHello.cpp.o"
+  "hiSLAM.pdb"
+  "hiSLAM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hiSLAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
